1a984819353589e45ded07fd28e14ee6556c2fe1
José Carlos da Silva Júnior <carlosiilva66@gmail.com>
2025-12-09 13:52:48 -0300

    chore: adicionar estrutura inicial do repositório e arquivos base

---
+++ linguagens/cpp/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/** Permite usar comandos como 'cout' e 'cin' diretamente. **/
+using namespace std;
+
+/**
+ * Função principal, onde a execução do programa começa.
+ */
+int main() {
+
+    /** Exemplo de saída: **/
+    cout << "Hello World!" << endl;
+
+    /** Retorno 0 indica que o programa terminou com sucesso. **/
+    return 0;
+}
---
+++ linguagens/java/Main.java
@@ -0,0 +1,21 @@
+package linguagens.java;
+
+import java.io.IOException;
+
+/**
+ * Classe principal do programa de exemplo.
+ * Responsável por executar a aplicação e imprimir uma mensagem no console.
+ */
+public class Main {
+
+    /**
+     * Método principal da aplicação.
+     * Exibe "Hello World!" no console.
+     *
+     * @param args argumentos da linha de comando (não utilizados).
+     * @throws IOException caso ocorra algum erro de entrada/saída.
+     */
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+    }
+}
